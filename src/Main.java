020e658c1724da214464982691a0561fc0c5d399
NotAWiz4rd <xamasa98@gmail.com>
2017-04-16 21:05:47 +0200

    SAI-31: basic structure

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by NotAWiz4rd on 16.04.2017.
  */
 public class Main
 {
   public static void main(String[] args)
+    throws IOException
   {
-    System.out.println("Hello World");
+    System.out.println("How many people should be generated?");
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    int input = Integer.parseInt(bufferedReader.readLine());
+    generatePeople(input);
+  }
+
+  private static void generatePeople(int count)
+  {
+    System.out.println(count + " people generated");
   }
 }
