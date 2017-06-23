3cb31c2e19dfa7a1a7ab31d1394de3911c7befad
notawiz4rd <xamasa98@gmail.com>
2017-06-23 16:33:45 +0200

    added writeToFile-method and improved code with nice comments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Random;
 
 /**
@@ -25,20 +27,27 @@ public class Main
   }
 
   private static void generatePeople(int count)
+    throws FileNotFoundException
   {
-    for(int i = 0; i < count; i++)
+    for(int i = 1; i < count + 1; i++) // starts counting at 1
     {
       System.out.println("Persona " + i);
       System.out.println("Name: " + generateName());
+      System.out.println("Sex: " + generateSex());
       System.out.println("Age: " + generateAge());
       System.out.println("Height: " + generateHeight());
-      System.out.println("Sex: " + generateSex());
       System.out.println("--------------------");
     }
-
   }
 
-  private static String generateName()
+  private static void writeToFile(String writable) // writes generated String into a file which is readable by the main application
+    throws FileNotFoundException
+  {
+    PrintWriter printWriter = new PrintWriter("definitions.people");
+    printWriter.print(writable);
+  }
+
+  private static String generateName()  // generates a random "name" which is a random configuration of chars
   {
     char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
     StringBuilder sb = new StringBuilder();
@@ -51,19 +60,19 @@ public class Main
     return sb.toString();
   }
 
-  private static int generateAge()
+  private static int generateAge() // generates an age between the set Max and Min age
   {
     Random random = new Random();
     return MAX_AGE - random.nextInt(MAX_AGE - MIN_AGE);
   }
 
-  private static int generateHeight()
+  private static int generateHeight() // generates a height between the set Max and Min height
   {
     Random random = new Random();
     return MAX_HEIGHT - random.nextInt(MAX_HEIGHT - MIN_HEIGHT);
   }
 
-  private static String generateSex()
+  private static String generateSex() // generates a sex as String
   {
     Random random = new Random();
     if(random.nextBoolean())
