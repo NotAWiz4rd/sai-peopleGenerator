671eb75ed5569266b00867b27dc08ba4d0cd0da2
notawiz4rd <xamasa98@gmail.com>
2017-06-23 16:54:43 +0200

    added automatically generating the people file from generated data and some new methods

--- src/Main.java
+++ src/Main.java
@@ -16,38 +16,50 @@ public class Main
   private static int MAX_HEIGHT = 205;
   private static int MIN_HEIGHT = 150;
 
+  private static PrintWriter printWriter;
+
   public static void main(String[] args)
     throws IOException
   {
+    printWriter = new PrintWriter("definitions.people");
+
     System.out.println("How many people should be generated?");
     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     int input = Integer.parseInt(bufferedReader.readLine());
+
     generatePeople(input);
     System.out.println(input + " people generated");
+
+    printWriter.close();
   }
 
   private static void generatePeople(int count)
     throws FileNotFoundException
   {
+    writeToFile("people");
+    writeToFile("{");
     for(int i = 1; i < count + 1; i++) // starts counting at 1
     {
+      writeToFile("{");
       System.out.println("Persona " + i);
       System.out.println("Name: " + generateName());
       System.out.println("Sex: " + generateSex());
       System.out.println("Age: " + generateAge());
       System.out.println("Height: " + generateHeight());
       System.out.println("--------------------");
+      writeToFile("}");
     }
+    writeToFile("}");
   }
 
   private static void writeToFile(String writable) // writes generated String into a file which is readable by the main application
     throws FileNotFoundException
   {
-    PrintWriter printWriter = new PrintWriter("definitions.people");
-    printWriter.print(writable);
+    printWriter.println(writable);
   }
 
-  private static String generateName()  // generates a random "name" which is a random configuration of chars
+  private static String generateName()
+    throws FileNotFoundException  // generates a random "name" which is a random configuration of chars
   {
     char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
     StringBuilder sb = new StringBuilder();
@@ -57,31 +69,62 @@ public class Main
       char c = chars[random.nextInt(chars.length)];
       sb.append(c);
     }
-    return sb.toString();
+    String name = sb.toString();
+    writeToFile("name: '" + name + "'");
+    return name;
   }
 
-  private static int generateAge() // generates an age between the set Max and Min age
+  private static int generateAge()
+    throws FileNotFoundException // generates an age between the set Max and Min age
   {
     Random random = new Random();
-    return MAX_AGE - random.nextInt(MAX_AGE - MIN_AGE);
+    int age = MAX_AGE - random.nextInt(MAX_AGE - MIN_AGE);
+    writeToFile("age: '" + age + "'");
+    return age;
   }
 
-  private static int generateHeight() // generates a height between the set Max and Min height
+  private static int generateHeight()
+    throws FileNotFoundException // generates a height between the set Max and Min height
   {
     Random random = new Random();
-    return MAX_HEIGHT - random.nextInt(MAX_HEIGHT - MIN_HEIGHT);
+    int height = MAX_HEIGHT - random.nextInt(MAX_HEIGHT - MIN_HEIGHT);
+    writeToFile("height: '" + height + "'");
+    return height;
   }
 
-  private static String generateSex() // generates a sex as String
+  private static String generateSex()
+    throws FileNotFoundException // generates a sex as String
   {
     Random random = new Random();
     if(random.nextBoolean())
     {
+      writeToFile("sex: 'male'");
       return "male";
     }
     else
     {
+      writeToFile("sex: 'female'");
       return "female";
     }
+  }
+
+  private static void generateAttributes()
+  {
+    // strength, charisma, intelligence, dexterity, constitution
+    // TODO think about max and min amounts of attribute points to be spend here randomly
+  }
+
+  private static void generateProperties()
+    throws FileNotFoundException
+  {
+    writeToFile("properties");
+    writeToFile("{");
+    // TODO read properties-file and put them into an Array, than generate a random amount of random numbers to choose the properties
+    writeToFile("}");
+  }
+
+  private static void generateOpinions()
+  {
+    // TODO read stored properties and choose random ones and generate a random impact and personalLevel
   }
 }
