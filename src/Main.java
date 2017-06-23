be5f83ba883937e2eb7fe8f50097f0158eabc94b
notawiz4rd <xamasa98@gmail.com>
2017-06-23 15:44:39 +0200

    added basic functionality to generate People with Name, age and height and display them

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Random;
 
 /**
  * Created by NotAWiz4rd on 16.04.2017.
@@ -21,7 +22,37 @@ public class Main
   {
     for(int i = 0; i < count; i++)
     {
-
+      System.out.println("Persona " + i);
+      System.out.println("Name: " + generateName());
+      System.out.println("Age: " + generateAge());
+      System.out.println("Height: " + generateHeight());
+      System.out.println("--------------------");
     }
+
+  }
+
+  private static String generateName()
+  {
+    char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    StringBuilder sb = new StringBuilder();
+    Random random = new Random();
+    for(int i = 0; i < random.nextInt(17) + 3; i++)
+    {
+      char c = chars[random.nextInt(chars.length)];
+      sb.append(c);
+    }
+    return sb.toString();
+  }
+
+  private static int generateAge()
+  {
+    Random random = new Random();
+    return 70 - random.nextInt(60);
+  }
+
+  private static int generateHeight()
+  {
+    Random random = new Random();
+    return 205 - random.nextInt(55);
   }
 }
